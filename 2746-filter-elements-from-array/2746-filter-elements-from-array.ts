60c0eba62de300e7c81df3dbaba5f31e7ffb8d2c
Inho Kang <85728616+ikang9712@users.noreply.github.com>
2023-09-01 06:33:34 +0900

    Time: 52 ms (84.91%), Space: 43.5 MB (25.21%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.ts
@@ -0,0 +1,7 @@
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    let res = []
+    for (let i = 0; i < arr.length; i++){
+        if (fn(arr[i],i)) res.push(arr[i])
+    }
+    return res
+};
